7b45271eb0dacf4f49a63a1762cb8f86e285781d
Utsav Bhatt <bhattutsav96@gmail.com>
2021-10-10 14:47:55 +0530

    Authentication code added

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        // private readonly ILogger<BaseApiController> _logger;
+
+        // public BaseApiController(ILogger<BaseApiController> logger)
+        // {
+        //     _logger = logger;
+        // }
+    }
+}
